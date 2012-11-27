4b6c1f4c6de2066721adcb41d00288ea2baa5179
Kenny Chan Zhi Yao <zhiyao.chan@gmail.com>
2012-11-27 11:18:50 +0800

    modify pages_in_footer after my customize override

--- app/overrides/pages_in_footer.rb
+++ app/overrides/pages_in_footer.rb
@@ -1,4 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/shared/_footer",
                      :name => "pages_in_footer",
                      :insert_bottom => "#footer-right",
+                     :sequence => {:after => "modify_footer"},
                      :partial => "spree/static_content/static_content_footer")
